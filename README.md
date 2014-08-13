78767d157f668cdca681b574c3a0378ffee03052
yangboz <YoungWelle@gmail.com>
2014-08-13 21:33:51 +0800

    ADDED:barebone project and files

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 SwiftSteeringBehavior
 =====================
 
-Steering behavior example at &lt;&lt;AdvanceEDActionScriptAnimation>> with abstracted interfaces and marshaling implementation by Swift language.  https://github.com/yangboz/as3SteeringBehavior
+Steering behavior example at &lt;&lt;AdvanceEDActionScriptAnimation>> with abstracted interfaces and marshaling implementation by Swift language. 
+
+Reference
+=====================
+
+http://www.raywenderlich.com/76147/uikit-dynamics-tutorial-swift
+
+https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-XID_449
+
+http://java.dzone.com/articles/using-swift-and-sprite-kit?mz=27249-mobile
---
+++ SwiftSteeringBehaviorDemo/SwiftSteeringBehaviorDemo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  SwiftSteeringBehaviorDemo
+//
+//  Created by yangboz on 14-8-7.
+//  Copyright (c) 2014年 GODPAPER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100));
+        square.backgroundColor = UIColor.grayColor();
+        view.addSubview(square);
+        //
+        var animator:UIDynamicAnimator!;
+        var gravity:UIGravityBehavior!;
+        //
+        animator = UIDynamicAnimator(referenceView: view);
+        gravity = UIGravityBehavior(items: [square]);
+        animator.addBehavior(gravity);
+        //
+        let barrier = UIView(frame: CGRect(x: 0, y: 300, width: 130, height: 20));
+        barrier.backgroundColor = UIColor.redColor();
+        view.addSubview(barrier);
+        //Handle collision
+        var collision:UICollisionBehavior!;
+        collision = UICollisionBehavior(items: [square,barrier]);
+        collision.translatesReferenceBoundsIntoBoundary = true;
+        animator.addBehavior(collision);
+        //
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
